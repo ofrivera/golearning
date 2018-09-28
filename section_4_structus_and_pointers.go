3c0ee5061f234c3fe2b5501ff152332b53611bde
orivera <orivera@localhost.localdomain>
2018-09-27 19:09:02 -0700

    4 and 5

---
+++ section_4_structus_and_pointers.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+)
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+	//contact   contactInfo
+}
+
+func main() {
+	// alex := person{"Alex", "Anderson"}
+	// alex := person{firstName: "Alex", lastName: "Anderson"}
+	// fmt.Println(alex)
+	// var alex person
+	// alex.firstName = "Alex"
+	// alex.lastName = "Anderson"
+	// fmt.Println(alex)
+	// fmt.Printf("%+v", alex)
+	// fmt.Println(" ")
+	//-----
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Part",
+		contactInfo: contactInfo{
+			email:   "jim@jim.com",
+			zipCode: 94000,
+		},
+	}
+	// fmt.Printf("%+v", jim)
+	//& operator (give me memory address)
+	jimPointer := &jim
+	jimPointer.updateName("Jimmy The Pos")
+	jim.print()
+	fmt.Println(" ")
+}
+
+//* operator (value of the memory address)
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
